542a8239039ec41c20fb30aa952529e9e3aa52a5
rhiannoncduvall@gmail.com <rhiannoncduvall@gmail.com>
2020-05-11 12:38:36 -0700

    icon changed to logo

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
-  manifestIcon: 'src/assets/img/logo_white.png',
+  manifestIcon: 'src/assets/img/vr-element_white.png',
   pathPrefix: `/gatsby-starter-spectral/`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'VR Toby',
   subHeading: 'Text Processing & Prediction Through Machine Learning',
